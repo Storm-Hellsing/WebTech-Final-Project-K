29af29bad49653f9bbc17cf9736f184aae7f8033
Storm-Hellsing <sg.abid@hotmail.com>
2023-04-25 16:25:38 +0600

    Woked with all user type hompages.

--- views/homePage_Admin.php
+++ views/homePage_Admin.php
@@ -2,12 +2,15 @@
 
     session_start();
     require_once("../models/user_all_model.php");
+    require_once("../models/product_all_model.php");
+    require_once("../models/product_image_all.php");
 
     if(isset($_COOKIE['userLoggedIn']))
     {
         $userID =  $_COOKIE['userLoggedIn'];
 
         $userData = fetch_Data_ByID($userID);
+        $found = find_product_all_general();
 ?>
 
 <html>
@@ -27,6 +30,68 @@
             <a href="" target="_blank" class="menu">Settings</a>
             <a href="" target="_blank" class="menu">My profile</a>
             <a href="logout.php" class="menu">Logout</a>
+        </div>
+
+        <div class="categories">
+            <p>Just for You</p>
+        </div>
+        <div class="container-box">
+            <?php 
+            
+                if($found > 0)
+                {
+                    $data = fetch_product_all_general();
+
+                    while($productData = mysqli_fetch_assoc($data))
+                    {
+                        $productID = $productData['product_id'];
+                        $merchantID = $productData['merchant_id'];
+                        $prodcutIMG_Data = fetch_image_by_product_id($productID);
+            ?>
+            <div class="content-box">
+                <img src="../assets/server_uploads/merchant_uploads/<?php echo($merchantID); ?>/products/<?php echo($productID); ?>/<?php echo($prodcutIMG_Data['product_img_1']); ?>" id="product-image">
+                <div id="labels">
+                    <p class="label"><?php echo($productData['product_name']); ?></p>
+                    <p class="label">Tk: <?php echo($productData['product_price']); ?></p>
+                </div>
+            </div>
+            <?php
+
+                    }
+                }
+
+            ?>
+        </div>
+        
+        <div class="categories">
+            <p>Electornics</p>
+        </div>
+        <div class="container-box">
+            <?php 
+            
+                if($found > 0)
+                {
+                    $data = fetch_product_all_general();
+
+                    while($productData = mysqli_fetch_assoc($data))
+                    {
+                        $productID = $productData['product_id'];
+                        $merchantID = $productData['merchant_id'];
+                        $prodcutIMG_Data = fetch_image_by_product_id($productID);
+            ?>
+            <div class="content-box">
+                <img src="../assets/server_uploads/merchant_uploads/<?php echo($merchantID); ?>/products/<?php echo($productID); ?>/<?php echo($prodcutIMG_Data['product_img_1']); ?>" id="product-image">
+                <div id="labels">
+                    <p class="label"><?php echo($productData['product_name']); ?></p>
+                    <p class="label">Tk: <?php echo($productData['product_price']); ?></p>
+                </div>
+            </div>
+            <?php
+
+                    }
+                }
+
+            ?>
         </div>
     </body>
 </html>
